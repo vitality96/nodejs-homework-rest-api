6fe17a2c78edbdf7261836459f4982ab280fafa8
Vitalii Peresoliak <vitalik21022@gmail.com>
2022-08-09 22:10:57 +0300

    update hm02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,65 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
+const path = require('path');
+const { v4 } = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, './contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === id);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
 
-const updateContact = async (contactId, body) => {}
+  const [removeContact] = contacts.splice(idx, 1);
+  await updateContacts(contacts);
+  return removeContact;
+};
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+    const newContact = {id: v4(), ...body};
+    contacts.push(newContact);
+    await updateContacts(contacts)
+    return newContact;
+}
+
+const updateContact = async (id, body) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { id, ...body};
+  await updateContacts(contacts);
+  return contacts[idx];
+};
+
+
+const updateContacts = async (contact) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contact));
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
+  updateContacts,
+  updateContact
 }
